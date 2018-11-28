857feb090fe18ab6c6c500c9791dc8194d9bfbbd
Alisha Tapiawala <art160630@utdallas.edu>
2018-11-28 14:59:02 -0600

    i don't even know whats going on anymore. can at least get the attendance history page to show up without crashing, still does not work

--- TeamNahhSEProject/app/src/main/java/com/example/android/teamnahhseproject/AttendanceHistoryStudent.java
+++ TeamNahhSEProject/app/src/main/java/com/example/android/teamnahhseproject/AttendanceHistoryStudent.java
@@ -1,45 +1,42 @@
 package com.example.android.teamnahhseproject;
 
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
-//import com.google.firebase.database.DataSnapshot;
-//import com.google.firebase.database.DatabaseError;
-//import com.google.firebase.database.DatabaseReference;
-//import com.google.firebase.database.FirebaseDatabase;
-//import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.database.*;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.*;
 
 public class AttendanceHistoryStudent extends AppCompatActivity {
 
     FirebaseDatabase database = FirebaseDatabase.getInstance();
-    FirebaseAuth authentication = FirebaseAuth.getInstance();
-    DatabaseReference referenceVar = database.getReference("student_users/"+authentication.getUid()+"/Classes/CS_3354_003");
-    String attendanceText;
-    TextView t = (TextView) findViewById(R.id.textBox);
+    FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+    DatabaseReference referenceVar = database.getReference("student_users/"+firebaseAuth.getUid()+"/Classes/CS_3354_003");
+    String attendance_text;
+    TextView textBox;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_attendance_history_student);
 
-      referenceVar.addValueEventListener(new ValueEventListener() {
+        textBox = findViewById(R.id.attendance);
+
+        referenceVar.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
-                    attendanceText = postSnapshot.getKey();
-                    t.append(attendanceText + "\t");
-                    attendanceText = postSnapshot.getValue(String.class);
-                    t.append(attendanceText + "\n");
+                    attendance_text = postSnapshot.getKey();
+                    textBox.append(attendance_text + "\t");
+                    attendance_text = postSnapshot.getValue(String.class);
+                    textBox.append(attendance_text + "\n");
                 }
             }
             @Override
             public void onCancelled(DatabaseError databaseError) { }
         });
-
     }
-
 }
