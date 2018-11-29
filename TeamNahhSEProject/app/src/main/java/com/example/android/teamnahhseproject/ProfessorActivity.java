34378121777212f2c689cd6a58fcf2764df38681
Alisha Tapiawala <art160630@utdallas.edu>
2018-11-28 20:10:56 -0600

    more progress on professorActivity

--- TeamNahhSEProject/app/src/main/java/com/example/android/teamnahhseproject/ProfessorActivity.java
+++ TeamNahhSEProject/app/src/main/java/com/example/android/teamnahhseproject/ProfessorActivity.java
@@ -5,11 +5,42 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class ProfessorActivity extends AppCompatActivity {
+    private Button attendanceHistory, qrGenerator, classes;
+    private TextView currentClass;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_professor);
+
+        attendanceHistory = (Button) findViewById(R.id.attendance_history);
+        qrGenerator = (Button) findViewById(R.id.generate_qr);
+        classes = (Button) findViewById(R.id.choose_class);
+        currentClass = (TextView) findViewById(R.id.current_class);
+
+        attendanceHistory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        qrGenerator.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        classes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
