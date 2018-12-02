5bf2814391a5d685e9a033d120e689dab99c76c1
hlateef17 <hzl170030@utdallas.edu>
2018-12-02 10:22:35 -0600

    added new key value for <current class> under user

--- TeamNahhSEProject/app/src/main/java/com/example/android/teamnahhseproject/User.java
+++ TeamNahhSEProject/app/src/main/java/com/example/android/teamnahhseproject/User.java
@@ -1,13 +1,14 @@
 package com.example.android.teamnahhseproject;
 
 public class User {
-    public String name, email;
+    public String name, email, current_class;
 
     public User(){ }
 
     public User(String name, String email) {
         this.name = name;
         this.email = email;
+        this.current_class = "-";
     }
 
 }
